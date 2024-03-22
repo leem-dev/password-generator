59489c05503dd115a89e773f00b4c125e10c52db
leem-dev <alabitseleem@gmail.com>
2024-03-23 00:37:10 +0100

    password gen

--- src/password-component.js
+++ src/password-component.js
@@ -2,9 +2,30 @@ import React, { useState } from "react";
 import cryptoRandomString from "crypto-random-string";
 
 const PasswordGenerator = () => {
+  const [password, setPassword] = useState("");
+  const [length, setLength] = useState(12);
+
+  const generatedPassword = () => {
+    const newPassword = cryptoRandomString({
+      length: length,
+      type: "ascii-printable",
+    });
+    setPassword(newPassword);
+  };
   return (
     <div>
-      <div>do</div>
+      <fieldset>
+        <legend>Password Generator</legend>
+        <label htmlFor="length">Length:</label>
+        <input
+          type="number"
+          id="length"
+          value={length}
+          onChange={(e) => setLength(parseInt(e.target.value))}
+        />
+        <button onClick={generatedPassword}>Generate Password</button>
+        <input type="text" value={password} readOnly />
+      </fieldset>
     </div>
   );
 };
