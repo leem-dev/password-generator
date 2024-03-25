316a4e882b14bae8957dc1c57d5f380494aa1c86
leem-dev <alabitseleem@gmail.com>
2024-03-25 23:01:04 +0100

    styling

--- src/password-component.js
+++ src/password-component.js
@@ -12,19 +12,29 @@ const PasswordGenerator = () => {
     });
     setPassword(newPassword);
   };
+
   return (
-    <div>
-      <fieldset>
+    <div className="password-container">
+      <fieldset className="password-field">
         <legend>Password Generator</legend>
-        <label htmlFor="length">Length:</label>
-        <input
-          type="number"
-          id="length"
-          value={length}
-          onChange={(e) => setLength(parseInt(e.target.value))}
-        />
-        <button onClick={generatedPassword}>Generate Password</button>
-        <input type="text" value={password} readOnly />
+        <section>
+          <label htmlFor="length">Length:</label>
+          <input
+            type="number"
+            id="length"
+            value={length}
+            onChange={(e) => setLength(parseInt(e.target.value))}
+          />
+        </section>
+        <section className="button-section">
+          <button onClick={generatedPassword}>Generate Password</button>
+          <button onClick={() => setPassword("")}>Reset</button>
+        </section>
+
+        <section>
+          <label htmlFor="password">Password:</label>
+          <input type="text" value={password} readOnly />
+        </section>
       </fieldset>
     </div>
   );
